8d687d5217d551f14f9160e3830b4e6116ba618a
gex <gex@yijiaoche.com>
2013-10-20 23:05:08 +0800

    1.页面最后加入一个隐藏的登陆后台地址链接

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 </div></div><!-- #content -->
 <div id="footer">
 	<div class="container">
-		<p>©2013<?php bloginfo('name'); ?> <?php bloginfo('description'); ?>。</p>
+		<p>©2013<?php bloginfo('name'); ?> <?php bloginfo('description'); ?>。<a href="<?php echo(admin_url()); ?>">&emsp;&emsp;</a></p>
 	</div>
 </div><!-- #footer -->
 <?php wp_footer(); ?>
