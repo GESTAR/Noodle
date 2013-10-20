63c90d3cbd94147cf46ce7fc30347e1f37d6d4df
gex <gex@yijiaoche.com>
2013-10-20 15:59:28 +0800

    Noodle v1.0.0

---
+++ footer.php
@@ -0,0 +1,9 @@
+</div></div><!-- #content -->
+<div id="footer">
+	<div class="container">
+		<p>©2013<?php bloginfo('name'); ?> <?php bloginfo('description'); ?>。</p>
+	</div>
+</div><!-- #footer -->
+<?php wp_footer(); ?>
+</body>
+</html>
